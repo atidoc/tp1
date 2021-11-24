4e1e64813bd9973b729aead3864df90202600376
atissot <arthur.tissot@onefit-medical.com>
2021-11-24 16:47:19 +0100

    Fix: substraction

--- monoappli/src/test/java/com/objis/mavenapps/AppTest.java
+++ monoappli/src/test/java/com/objis/mavenapps/AppTest.java
@@ -4,7 +4,7 @@ public class MaClasseTest extends TestCase {
     public void testSum() throws Exception {
         assertEquals(2, MaClasse.sum(1,1));
     }
-    public void testSum() throws Exception {
-        assertEquals(0, MaClasse.sum(1,1));
+    public void testSub() throws Exception {
+        assertEquals(0, MaClasse.sub(1,1));
     }
 }
